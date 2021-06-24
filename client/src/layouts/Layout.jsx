b1c7cd81c83776f18d8d039771f0a198dcf537a1
jbenet <nycflasher@yahoo.com>
2021-06-24 15:17:39 -0400

    more auth app layout login register

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -2,14 +2,21 @@ import { Link } from 'react-router-dom'
 
 
 export default function Layout(props) {
+  const { currentUser, handleLogout, children } = props;
   return (
     <div>
       <header>
         <h1>WhatsGoinOn</h1>
+        {currentUser ? (
+					<>
+						<p>{currentUser.username}</p>
+						<button onClick={handleLogout}>Logout</button>
+					</>
+				) : (
         <Link to='/login'>Login/Register</Link>
-
+        )}
       </header>
-      {props.children}
+      {children}
 
       
     </div>
