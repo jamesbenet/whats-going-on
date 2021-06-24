f3eefe6d2300349d7b60da252a3d551773f56b3d
jbenet <nycflasher@yahoo.com>
2021-06-24 09:03:35 -0400

    FE structure and services

---
+++ client/src/services/events.js
@@ -0,0 +1,31 @@
+import api from './api-config';
+
+export const getAllEvents = async () => {
+	const resp = await api.get('/events');
+	return resp.data;
+};
+
+export const getOneEvent = async (id) => {
+	const resp = await api.get(`/events/${id}`);
+	return resp.data;
+};
+
+export const postEvent = async (eventData) => {
+	const resp = await api.post('/events', { event: eventData });
+	return resp.data;
+};
+
+export const putEvent = async (id, eventData) => {
+	const resp = await api.put(`/events/${id}`, { event: eventData });
+	return resp.data;
+};
+
+export const deleteEvent= async (id) => {
+	const resp = await api.delete(`/events/${id}`);
+	return resp;
+};
+
+export const addEvent = async (eventId, commentId) => {
+	const resp = await api.put(`/comments/${commentId}/events/${eventId}`);
+	return resp.data;
+};
