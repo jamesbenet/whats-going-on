b97a8f0dffcc4dafdd5ca695b8b78626c0a105b1
jbenet <nycflasher@yahoo.com>
2021-06-25 16:13:28 -0400

    Events and main con basic functionality

--- client/src/containers/MainContainer.jsx
+++ client/src/containers/MainContainer.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-export default function MainContainer () {
+import Events from '../screens/Events';
+
+import { getAllEvents } from '../services/events';
+
+import './MainContainer.css'
+
+export default function MainContainer() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+		const fetchEvents = async () => {
+			const eventList = await getAllEvents();
+			setEvents(eventList);
+		};
+		fetchEvents();
+	}, []);
   return (
     <div>
       <h1>Main Con</h1>
+      <Switch>
+      <Route path='/events'>
+					<Events events={events} />
+			</Route>
+      </Switch>
+      
     </div>
+
   );
 };
 
