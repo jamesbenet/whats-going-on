17e9f84de92ae4dddfcb916a84deb4d0ecb4ed63
jbenet <nycflasher@yahoo.com>
2021-06-29 09:05:13 -0400

    redirects

--- client/src/screens/Events/Events.jsx
+++ client/src/screens/Events/Events.jsx
@@ -1,26 +1,32 @@
-import "./Events.css"
-import { Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import "./Events.css";
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 export default function Events(props) {
-  const { events } = props
-  
-	return (
-		<div className="events-list-page">
+  const { events } = props;
+
+  return (
+    <div className="events-list-page">
       <h1>Upcoming Events</h1>
-      <Link to='/events/new'>
-				<button>Add a new event</button>
-			</Link>
-      
-			{events.map((event) => (
-				<Fragment key={event.id}>
-					<Link to={`/events/${event.id}`}>
-            <div>{event.performer_name}
-            {event.date}
-            {event.venue_location}</div>
-					</Link>
-				</Fragment>
-			))}
-		</div>
-	);
+      <Link to="/events/new">
+        <button>Add a new event</button>
+      </Link>
+
+      {events.map((event) => (
+        <Fragment key={event.id}>
+          <Link to={`/events/${event.id}`}>
+            <div classname="list-row">
+              <div className="event-performer-wrapper">
+                {event.performer_name}
+              </div>
+              <div className="event-date-wrapper">{event.date}</div>
+              <div className="event-location-wrapper">
+                {event.venue_location}
+              </div>
+            </div>
+          </Link>
+        </Fragment>
+      ))}
+    </div>
+  );
 }
